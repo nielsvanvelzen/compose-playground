52b3bade753792d50bc1d79dee89d9640979ba99
Niels van Velzen <git@ndat.nl>
2022-05-31 16:08:54 +0200

    Add androidx.navigation

--- app/src/main/kotlin/Activity.kt
+++ app/src/main/kotlin/Activity.kt
@@ -1,15 +1,32 @@
 package nl.ndat.compose.playground
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Card
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 class Activity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +39,47 @@ class Activity : ComponentActivity() {
 @Preview
 @Composable
 fun AppView() = MaterialTheme {
-	Card {
-		Text("Hello")
+	val navController = rememberNavController()
+	val scaffoldState = rememberScaffoldState()
+	val scope = rememberCoroutineScope()
+
+	Scaffold(
+		scaffoldState = scaffoldState,
+		topBar = {
+			TopAppBar {
+				IconButton(onClick = { scope.launch { scaffoldState.drawerState.apply { if (isClosed) open() else close() } } }) {
+					Icon(Icons.Filled.Menu, null)
+				}
+				Text(stringResource(R.string.app_name))
+			}
+		},
+		bottomBar = { AppBottomNavigationView(navController) },
+		drawerContent = { AppContentView(navController) },
+		content = { AppContentView(navController) }
+	)
+}
+
+@Composable
+fun AppBottomNavigationView(navController: NavHostController) {
+	BottomNavigation {
+		BottomNavigationItem(
+			icon = { Icon(Icons.Filled.Favorite, null) },
+			selected = navController.currentDestination?.route == "favorites",
+			onClick = { navController.navigate("favorites") }
+		)
+
+		BottomNavigationItem(
+			icon = { Icon(Icons.Filled.Home, null) },
+			selected = navController.currentDestination?.route == "home",
+			onClick = { navController.navigate("home") }
+		)
+	}
+}
+
+@Composable
+fun AppContentView(navController: NavHostController) {
+	NavHost(navController, "home") {
+		composable("home") { Text("Home") }
+		composable("favorites") { Text("FAVORITES") }
 	}
 }
